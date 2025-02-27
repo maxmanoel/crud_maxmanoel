5eb8f5100ab42f39a40b9e37496c79fc34f99d46
maxmanoel <max329900@gmail.com>
2025-02-26 21:00:31 -0300

    alteracao

---
+++ crud.py
@@ -0,0 +1,56 @@
+import mysql.connector
+from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE
+
+def get_connection():
+    return mysql.connector.connet(
+    host = MYSQL_HOST,
+    user = MYSQL_USER,
+    password = MYSQL_PASSWORD,
+    database = MYSQL_DATABASE,
+
+    )
+
+def create_user(nome, telefone, email, usuario, senha):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "inserir usuario (nome, telefone, email, usuario, senha) VALUES (%s,%s,%s,%s) )"
+
+    cursor.execute(query,[nome,telfone,email,usuario,senha])
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def read_users():
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "SELECT * FROM aluno )"
+
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return result
+
+
+def uptade_user(user_id,nome,telefone,email,usuario,senha):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = " UPTADE aluno nome=%s, telefone=%s, email=%s, usuario=%s, senha=%s WHERE id usuario=%s) "
+
+    cursor.execute(query,[user_id,nome,telefone,email,usuario,senha])
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
+
+def delete_user(user_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    query = "DELETE FROM  usuario WHERE idusuario =%s) "
+
+    cursor.execute(query,[user_id])
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+#pip install mysql-connector-python
---
+++ tela.py
@@ -0,0 +1,95 @@
+import tkinter as tk
+from tkinter import messagebox
+from crud import create_user,read_users,update_user,delete_user
+
+class CRUDApp:
+    def __init__(self,root):
+        self.root = root
+        self.root.title("CRUD USUARIOS ")
+
+# Criaçao de WIDGETS
+        self.create.widgets()
+
+def create_widgets(self):
+# Labels
+    tk.Label(self.root,text="Nome : ").grid(row=0,column=0)
+    tk.Label(self.root,text="Telefone : ").grid(row=1,column=0)
+    tk.Label(self.root,text="Email : ").grid(row=2,column=0)
+    tk.Label(self.root,text="Usuario : ").grid(row=3,column=0)
+    tk.Label(self.root,text="Senha : ").grid(row=4,column=0)
+
+    tk.Label(self.root,text="User ID (for update/delete) : ").grid(row=5,column=0)
+
+# Criar as caixas para digitar os valores
+    self.nome_entry = tk.Entry(self.root).grid(row=0,column=1)
+    self.telefone_entry = tk.Entry(self.root).grid(row=1,column=1)
+    self.email_entry = tk.Entry(self.root).grid(row=2,column=1)
+    self.usuario_entry = tk.Entry(self.root).grid(row=3,column=1)
+    self.senha_entry = tk.Entry(self.root).grid(row=4,column=1)
+
+    self.user_id_entry = tk.Entry(self.root).grid(row=5,column=1)
+
+# Botoes do Crud
+    tk.Button(self.root,text="Cria usuario",command=self.create_user).grid(row=6,column=0,columnspan=1)
+    tk.Button(self.root,text="Listar usuario",command=self.create_user).grid(row=6,column=1,columnspan=1)
+    tk.Button(self.root,text="Alterar usuario",command=self.create_user).grid(row=7,column=0,columnspan=1)
+    tk.Button(self.root,text="Excluir usuario",command=self.create_user).grid(row=7,column=1,columnspan=1)
+
+def create_user(self):
+    nome = self.nome_entry.get()
+    telefone = self.telefone_entry.get()
+    email = self.email_entry.get()
+    usuario = self.usuario_entry.get()
+    senha = self.senha_entry.get()
+
+if nome and telefone and email and usuario and self :
+    create_user(nome,telefone,email,usuario,senha)
+    self.nome_entry.delete(0,tk.END)
+    self.telefone_entry.delete(0,tk.END)
+    self.email_entry.delete(0,tk.END)
+    self.usuario_entry.delete(0,tk.END)
+    self.senha_entry.delete(0,tk.END)
+    messagebox.showerror("Success","Usuario criado com secesso")
+else:
+    messagebox.showerror("Error","Todos os campos são obrigatórios")
+
+
+def read_users(self):
+    users = read_users()
+    self.text_area.delete(1.0,tk.END)
+    for user in users:
+        self.text_area.insert(tk.END,f"ID: {user}, Nome:{user}, Telefone:{user}, Email:{user}\n")
+
+
+def update_user(self):
+    user_id = self.user_id_entry.get()
+    nome = self.nome_entry.get()
+    telefone = self.telefone_entry.get()
+    email = self.email_entry.get()
+    usuario = self.usuario_entry.get()
+    senha = self.senha_entry.get()
+
+if user_id and nome and telefone and email and usuario and senha :
+    update_user(user_id,nome,telefone,email,usuario,senha)
+    self.nome_entry.delete(0,tk.END)
+    self.telefone_entry.delete(0,tk.END)
+    self.email_entry.delete(0,tk.END)
+    self.usuario_entry.delete(0,tk.END)
+    self.senha_entry.delete(0,tk.END)
+    messagebox.showerror("Success","Usuario alterado com secesso")
+else:
+    messagebox.showerror("Error","Todos os campos são obrigatórios")
+
+def delete_user(self):
+    user_id = self.user_id_entry.get()
+if user_id:
+    delete_user(user_id)
+    self.user_id_entry.delete(0, tk.END)
+    messagebox.showerror("Success", "Usuario excluído com sucesso.")
+else:
+    messagebox.showerror("Error", "ID do usuario é o obrigatório")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = CRUDApp(root)
+    root.mainloop()
