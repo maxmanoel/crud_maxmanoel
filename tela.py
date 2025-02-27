f1a6643aa6515470ee5bb37460f72cbaf1f785d4
maxmanoel <max329900@gmail.com>
2025-02-26 22:01:09 -0300

    alteracao

--- tela.py
+++ tela.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-from crud import create_user, read_users, update_user, delete_user
+from crud import create_user,read_users,update_user,delete_user
 
 class CRUDapp:
     def __init__(self, root):
